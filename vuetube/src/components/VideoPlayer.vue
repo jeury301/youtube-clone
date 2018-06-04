8f67812e9dea60b36fa587f09d7b88a0e1c262b6
Jeury Mejia <jeury.mejia@gmail.com>
2018-06-03 21:19:13 -0400

    Add own content to playlist

--- vuetube/src/components/VideoPlayer.vue
+++ vuetube/src/components/VideoPlayer.vue
@@ -27,45 +27,54 @@
 let videos = [
   {
     id: 1,
-    title: '18-core iMac Pro Review: Not a Trap!',
-    thumbnail: 'https://i.ytimg.com/vi/jn9mHzXJIV0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvJvk4k_UNB9nst4pFP-txM1TLZA',
-    youtubeURL: 'https://www.youtube.com/embed/jn9mHzXJIV0',
-    creator: 'Marques Brownlee',
+    title: 'Nameless Python Game - GamePlay',
+    thumbnail: 'https://img.youtube.com/vi/csoYfiCFju4/0.jpg',
+    youtubeURL: 'https://www.youtube.com/embed/csoYfiCFju4',
+    creator: 'Jeury Mejia',
     likes: 0,
     views: 0
   },
   {
     id: 2,
-    title: 'Dope Tech: Camera Robots!',
-    thumbnail: 'https://i.ytimg.com/vi/UIwdCN4dV6w/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDhlan32jHSvicGZezDFPjAOdXGUA',
-    youtubeURL: 'https://www.youtube.com/embed/UIwdCN4dV6w',
-    creator: 'Marques Brownlee',
+    title: 'EggTimer - macOS development tutorial result',
+    thumbnail: 'https://img.youtube.com/vi/ck0gbznoJQw/0.jpg',
+    youtubeURL: 'https://www.youtube.com/embed/ck0gbznoJQw',
+    creator: 'Jeury Mejia',
     likes: 0,
     views: 0
   },
   {
     id: 3,
-    title: 'Let\'s Talk About Tesla Roadster 2020!',
-    thumbnail: 'https://i.ytimg.com/vi/ctx4YBEdOxo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBDnlrC2rVwXamNkicEEbc3Mf4T0w',
-    youtubeURL: 'https://www.youtube.com/embed/ctx4YBEdOxo',
-    creator: 'Marques Brownlee',
+    title: 'CS 490 Project - Release Version Demo (December 2017)',
+    thumbnail: 'http://img.youtube.com/vi/qNahJ7HPGjk/0.jpg',
+    youtubeURL: 'https://www.youtube.com/embed/qNahJ7HPGjk',
+    creator: 'Michael Anderson',
     likes: 0,
     views: 0
   },
   {
     id: 4,
-    title: 'Talking Tech with Neil deGrasse Tyson!',
-    thumbnail: 'https://i.ytimg.com/vi/pqQrL1K0Z5g/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLA5hTiwkz4Tr1w1hSMhPlwtmQeyYw',
-    youtubeURL: 'https://www.youtube.com/embed/pqQrL1K0Z5g',
-    creator: 'Marques Brownlee',
+    title: 'Classmo - Final project for CS491 (capstone)',
+    thumbnail: 'http://img.youtube.com/vi/Z0_JhfaLb5o/0.jpg',
+    youtubeURL: 'https://www.youtube.com/embed/Z0_JhfaLb5o',
+    creator: 'Jeury Mejia',
     likes: 0,
     views: 0
   },
   {
     id: 5,
-    title: 'The Apple Ecosystem: Explained!',
-    thumbnail: 'https://i.ytimg.com/vi/KB4_WIPE7vo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCCxXm7aoPShOwON74nhMlGYMUkHw',
-    youtubeURL: 'https://www.youtube.com/embed/KB4_WIPE7vo',
+    title: 'An NJIT Advisement system - CS370 Final Project',
+    thumbnail: 'http://img.youtube.com/vi/c6vjW5ljTJE/0.jpg',
+    youtubeURL: 'https://www.youtube.com/embed/c6vjW5ljTJE',
+    creator: 'Jeury Mejia',
+    likes: 0,
+    views: 0
+  },
+  {
+    id: 6,
+    title: '18-core iMac Pro Review: Not a Trap!',
+    thumbnail: 'https://i.ytimg.com/vi/jn9mHzXJIV0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvJvk4k_UNB9nst4pFP-txM1TLZA',
+    youtubeURL: 'https://www.youtube.com/embed/jn9mHzXJIV0',
     creator: 'Marques Brownlee',
     likes: 0,
     views: 0
