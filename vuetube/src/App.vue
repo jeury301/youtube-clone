d0d799e6ebd9c63f48b0bf638618d93cfe2b62fc
Jeury Mejia <jeury.mejia@gmail.com>
2018-06-03 20:09:30 -0400

    Create initial youtube-like screen. Add list of videos to watch to the right side of the screen. Add active video currently playing.

--- vuetube/src/App.vue
+++ vuetube/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <VideoPlayer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 import VideoPlayer from './components/VideoPlayer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     VideoPlayer
   }
 }
@@ -23,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
